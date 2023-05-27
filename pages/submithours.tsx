8b0720548edb5a2d7ffa766d32bdc40310b13010
Alexander Park <me@parkalex.dev>
2023-05-27 14:41:15 -0500

    file uploading

--- pages/submithours.tsx
+++ pages/submithours.tsx
@@ -1,23 +1,64 @@
-import { useSession, signIn, signOut } from "next-auth/react"
+import { useSession, signIn, signOut } from "next-auth/react";
+import { useState } from "react";
+import axios from "axios";
 
-export default function SubmitHours() {
-  const { data: session } = useSession()
+const BUCKET_URL = "https://cdn.funny-bunny.ninja/";
+
+export default function Home() {
+  const [file, setFile] = useState<any>();
+  const [uploadingStatus, setUploadingStatus] = useState<any>();
+  const [uploadedFile, setUploadedFile] = useState<any>();
+  const { data: session } = useSession();
+
+  const selectFile = (e) => {
+    setFile(e.target.files[0]);
+  };
+
+  const uploadFile = async () => {
+    setUploadingStatus("Uploading the file to AWS S3");
+
+    let { data } = await axios.post("/api/file", {
+      name: file.name,
+      type: file.type,
+    });
+
+    console.log(data);
+
+    const url = data.url;
+    let { data: newData } = await axios.put(url, file, {
+      headers: {
+        "Content-type": file.type,
+        "Access-Control-Allow-Origin": "*",
+      },
+    });
+
+    setUploadedFile(BUCKET_URL + file.name);
+    setFile(null);
+  };
+
   if (session) {
     return (
-      <>
-        Signed in as {session?.user?.email} <br />
-        <form action="/api/upload">
-          <input type="file" />
-          <input type="submit" />
-        </form>
-        
-      </>
-    )
+      <div className="container flex items-center p-4 mx-auto min-h-screen justify-center">
+        <main>
+          <p>Please select a file to upload</p>
+          <input type="file" onChange={(e) => selectFile(e)} />
+          {file && (
+            <>
+              <p>Selected file: {file.name}</p>
+              <button
+                onClick={uploadFile}
+                className=" bg-purple-500 text-white p-2 rounded-sm shadow-md hover:bg-purple-700 transition-all"
+              >
+                Upload a File!
+              </button>
+            </>
+          )}
+          {uploadingStatus && <p>{uploadingStatus}</p>}
+          {uploadedFile && <img src={uploadedFile} />}
+        </main>
+      </div>
+    );
+  } else {
+    return <h1>Login required</h1>;
   }
-  return (
-    <>
-      Access denied. Did you forget to sign in? <br />
-      <button onClick={() => signIn()}>Sign in</button>
-    </>
-  )
 }
