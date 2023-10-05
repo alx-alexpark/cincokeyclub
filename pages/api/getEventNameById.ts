88a870eea965a251a7e5aa6bf1ae58c7f697df87
supercolbat <supercolbat@protonmail.com>
2023-10-05 16:31:44 -0500

    refactor: extract getEventNameById function

--- pages/api/getEventNameById.ts
+++ pages/api/getEventNameById.ts
@@ -1,30 +1,18 @@
 import { NextApiRequest, NextApiResponse } from "next";
-import clientPromise from "@/lib/mongodb";
+import getEventNameById from "@/util/getEventNameById";
 
-export default async function getEventNameById(
+export default async function handler(
   req: NextApiRequest,
-  res: NextApiResponse,
+  res: NextApiResponse
 ) {
   try {
     // Get request parameters
     let { eventId } = req.query;
 
-    // Get client details
-    const client = await clientPromise;
-    const db = client.db("auth");
+    // Get event name
+    let eventName = getEventNameById(eventId as string);
 
-    // Find events
-    const collection = db.collection("events");
-    const allEvents = await collection.find().toArray();
-
-    // Find and return the first event that matches the event ID
-    for (let i = 0; i < allEvents.length; i++) {
-      if (allEvents[i].id === eventId) {
-        return res.json({ eventName: allEvents[i].name });
-      }
-    }
-
-    res.json({ eventName: "Event not found" });
+    res.json({ eventName: eventName });
   } catch (e) {
     res.json({ error: e });
   }
