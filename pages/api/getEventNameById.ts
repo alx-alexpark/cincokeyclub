e06bd251ac8a621a61f42a0a83afabc615104d4e
supercolbat <supercolbat@protonmail.com>
2023-10-05 16:51:54 -0500

    style: run formatter

--- pages/api/getEventNameById.ts
+++ pages/api/getEventNameById.ts
@@ -3,7 +3,7 @@ import getEventNameById from "@/util/getEventNameById";
 
 export default async function handler(
   req: NextApiRequest,
-  res: NextApiResponse
+  res: NextApiResponse,
 ) {
   try {
     // Get request parameters
--- pages/gm.tsx
+++ pages/gm.tsx
@@ -11,71 +11,66 @@ import { Flex, Text } from "@chakra-ui/react";
 import Link from "next/link";
 import Navbar from "@/components/Navbar";
 import { ToastContainer, toast } from "react-toastify";
-import 'react-toastify/dist/ReactToastify.css';
+import "react-toastify/dist/ReactToastify.css";
 
 export default function GeneralMeeting() {
   const { data: session, status } = useSession();
 
   const formik = useFormik({
     initialValues: {
-        code: ""
+      code: "",
     },
-    onSubmit: async (values: {
-        code: string;
-    }) => {
-       const callApi = async () => await axios.post("/api/gm", values); 
-       toast.promise(
-        callApi,
-        {
-          pending: 'Working',
-          success: 'It worked!!',
-          error: 'Code invalid or hours already claimed'
-        }
-    )
-     
+    onSubmit: async (values: { code: string }) => {
+      const callApi = async () => await axios.post("/api/gm", values);
+      toast.promise(callApi, {
+        pending: "Working",
+        success: "It worked!!",
+        error: "Code invalid or hours already claimed",
+      });
     },
   });
-  
+
   if (session) {
     return (
       <Flex minH="100vh" flexDir="column">
         <Navbar />
-      <div className="container flex items-center p-4 mx-auto flex-1 justify-center flex-col">
-        
-        <main
-          style={{ backgroundColor: "transparent", background: "transparent",}}
-        >
-
-        <ToastContainer />
-
-          <form
-            onSubmit={(e) => {
-              formik.handleSubmit(e);
+        <div className="container flex items-center p-4 mx-auto flex-1 justify-center flex-col">
+          <main
+            style={{
+              backgroundColor: "transparent",
+              background: "transparent",
             }}
-            className="flex flex-col"
           >
-            <label htmlFor="code" className="mt-2 font-semibold">
-              Enter the 6 digit code
-            </label>
-            <input
-              type="number"
-              id="code"
-              name="code"
-              maxLength={4}
-              required
-              onChange={formik.handleChange}
-              value={formik.values.code}
-              className="text-black rounded-md pl-2 p-2 text-md mb-2"
-            />
-            <input
-              type="submit"
-              style={{cursor: "pointer"}}
-              disabled={formik.values.code == "0" || formik.isSubmitting}
-              className="py-2 px-4 bg-blue-500 text-white font-semibold rounded-lg shadow-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-75"
-            />
-          </form>
-        </main>
-      </div>
+            <ToastContainer />
+
+            <form
+              onSubmit={(e) => {
+                formik.handleSubmit(e);
+              }}
+              className="flex flex-col"
+            >
+              <label htmlFor="code" className="mt-2 font-semibold">
+                Enter the 6 digit code
+              </label>
+              <input
+                type="number"
+                id="code"
+                name="code"
+                maxLength={6}
+                required
+                onChange={formik.handleChange}
+                value={formik.values.code}
+                className="text-black rounded-md pl-2 p-2 text-md mb-2"
+              />
+              <input
+                type="submit"
+                style={{ cursor: "pointer" }}
+                disabled={formik.values.code == "0" || formik.isSubmitting}
+                className="py-2 px-4 bg-blue-500 text-white font-semibold rounded-lg shadow-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-75"
+              />
+            </form>
+          </main>
+        </div>
       </Flex>
     );
   } else if (status == "loading") {
