ed5e3435a17697f9cab447f7fccc0ad7807f4903
Alexander Park <me@parkalex.dev>
2023-10-09 12:02:01 -0500

    Fix potential XSS and set limits on name length

--- pages/api/name.ts
+++ pages/api/name.ts
@@ -2,23 +2,34 @@ import { getServerSession } from "next-auth/next";
 import { authOptions } from "./auth/[...nextauth]";
 import { NextApiRequest, NextApiResponse } from "next";
 import clientPromise from "../../lib/mongodb";
+import { JSDOM } from 'jsdom';
+import DOMPurify from "dompurify";
+
+interface NameRequest {
+    name: string;
+}
 
 export default async function renameUser(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
   const session = await getServerSession(req, res, authOptions);
-  const { name } = req.body;
+  const { name }: NameRequest = req.body;
   if (session) {
     try {
+      const window = new JSDOM('').window;
+      const purify = DOMPurify(window);
       const client = await clientPromise;
       const db = client.db("auth");
       const usersCollection = db.collection("users");
+      if (name.length == 0 || name.length > 200) {
+        res.status(420).json({error: "What goofy things are you doing?"})
+      }
       usersCollection.updateOne(
         { email: session.user?.email },
-        { $set: { name: name } }
+        { $set: { name: purify.sanitize(name) } }
       );
-      res.status(200).json({ newName: name });
+      res.status(200).json({ newName: purify.sanitize(name) });
     } catch (e) {
       res.status(500).json({ error: e });
     }
